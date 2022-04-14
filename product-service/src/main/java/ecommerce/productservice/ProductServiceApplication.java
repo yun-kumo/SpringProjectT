ae5c7db7bd5135027d4fe1d81e5ffa0f9e24f6db
yun-kumo <yun_kumo@petalmail.com>
2022-04-14 21:52:06 +0800

    Default Changelist

--- product-service/src/main/java/ecommerce/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/ecommerce/productservice/ProductServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//test
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
